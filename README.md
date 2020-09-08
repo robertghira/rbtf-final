d89f70642e04b70e91b29e45381eb0d868bf438b
robertghira <ghirarobert@gmail.com>
2020-09-08 15:35:25 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# rbtf-final
---
+++ precache-manifest.1549b655d5971f1ddeb3809105b9a40c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe4f5fbe9a4334e78558fa0dbefff49",
+    "url": "/rbtf-final/index.html"
+  },
+  {
+    "revision": "8e49f947341c80211c29",
+    "url": "/rbtf-final/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed84be7a78125d9c8b47",
+    "url": "/rbtf-final/static/css/main.55e36c6d.chunk.css"
+  },
+  {
+    "revision": "8e49f947341c80211c29",
+    "url": "/rbtf-final/static/js/2.581309ac.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/rbtf-final/static/js/2.581309ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed84be7a78125d9c8b47",
+    "url": "/rbtf-final/static/js/main.acf8b5a8.chunk.js"
+  },
+  {
+    "revision": "5ae87f70d6e18eed057a",
+    "url": "/rbtf-final/static/js/runtime-main.db1f1b09.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/rbtf-final/static/media/SEGA.12a3db8b.woff"
+  }
+]);
